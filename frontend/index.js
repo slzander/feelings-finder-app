9316e1936a44b243970013ffcba2c81cf898022b
slzander <staceyzander@gmail.com>
2019-12-03 12:52:09 -0700

    adds front end and training page

--- frontend/index.js
+++ frontend/index.js
@@ -1,86 +1,55 @@
-let mobileNet;
+let mobilenet;
 let classifier;
 let video;
-let label = 'train me!';
-let happyButton;
-let sadButton;
-let trainButton;
-let saveButton;
+let label = 'loading model';
 
+// document.addEventListener("DOMContentLoaded", () => {
+    
 
-document.addEventListener("DOMContentLoaded", () => {
-    // const newEmotionButton = document.querySelector("newEmotion")
+// })
 
-    // newEmotionButton.addEventListener("click", function(event){
-
-    // })
-})
-
-function setup() {
-  createCanvas(320, 270);
-  video = createCapture(VIDEO);
-  video.hide();
-  background(0);
-  mobileNet = ml5.featureExtractor('MobileNet', modelReady);
-  classifier = mobileNet.classification(video, videoReady);
-
-  happyButton = createButton('happy');
-  happyButton.mousePressed(function() {
-    classifier.addImage('happy');
-  });
-
-  sadButton = createButton('sad');
-  sadButton.mousePressed(function() {
-    classifier.addImage('sad');
-  });
-
-  trainButton = createButton('train');
-  trainButton.mousePressed(function() {
-    classifier.train(whileTraining);
-  });
-
-  saveButton = createButton('save');
-  saveButton.mousePressed(function() {
-    classifier.save();
-  });
-}
 
 function modelReady() {
-    console.log('Model is ready!!!');
-    classifier.load('model.json', customModelReady)
+  console.log('Model is ready!!!')
+  classifier.load('model.json', customModelReady)
 }
 
 function customModelReady() {
-    console.log('Custom model is ready!!!');
+  console.log('Custom Model is ready!!!')
+  label = 'model ready'
+  classifier.classify(gotResults)
 }
 
 function videoReady() {
-    console.log('Video is ready!!!');
+  console.log('Video is ready!!!')
+}
+
+function setup() {
+  createCanvas(320, 270)
+  video = createCapture(VIDEO)
+  video.hide()
+  background(0)
+  mobilenet = ml5.featureExtractor('MobileNet', modelReady)
+  classifier = mobilenet.classification(video, videoReady)
+}
+
+function draw() {
+  background(0)
+  image(video, 0, 0, 320, 240)
+  fill(255)
+  textSize(16)
+  text(label, 10, height - 10)
 }
 
 function gotResults(error, result) {
   if (error) {
-    console.error(error);
+    console.error(error)
   } else {
-    label = result[0].label;
-    classifier.classify(gotResults);
+    label = result[0].label
+    classifier.classify(gotResults)
   }
 }
 
-function whileTraining(loss) {
-    if (loss == null) {
-        console.log('Training Complete');
-        classifier.classify(gotResults);
-    } else {
-        console.log(loss);
-    }
-}
 
-function draw() {
-  background(0);
-  image(video, 0, 0, 320, 240);
-  fill(255);
-  textSize(16);
-  text(label, 10, height - 10);
-}
+
 
